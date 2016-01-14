8af2d64adb1eabf29745fb2dd8a9d0d70259b541
Mike Allison <mike@mikeallisononline.com>
2016-01-13 21:03:54 -0600

    update resources

--- src/resources.component.ts
+++ src/resources.component.ts
@@ -6,6 +6,12 @@ import {Component} from "angular2/core";
 			<h3 class="display-3">Resources</h3>
 			<p class="lead"><a href="http://angular.io" target="_blank">angular.io</a></p>
 			<p class="lead"><a href="http://reddit.com/r/Angular2" target="_blank">reddit.com/r/Angular2</a></p>
+			<p class="lead">mikeallisononline.com</p>
+			<p class="lead">github.com/mikeallisonJS</p>
+			<p class="lead">github.com/mike-allison</p>
+			<p class="lead">@mikeallison</p>
+			<p class="lead">mike@mikeallisononline.com</p>
+
 	`
 })
 export default class ResourcesComponent {
